68b8ef6df87564549f57f89e73c9b7712c9b0490
Nicholas Marshall <nmarsha3@db.cse.nd.edu>
2021-04-06 18:28:21 -0400

    Added a simple query page

---
+++ frontend/lookup.php
@@ -0,0 +1,24 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$code = $_GET['input-code'];
+
+$link = mysqli_connect('localhost', 'mrauch2', 'frc254') or die ('died');
+
+mysqli_select_db($link, 'mrauch2');
+
+$stmt = mysqli_prepare($link, "select name from Procedures where code = ?");
+mysqli_stmt_bind_param($stmt, "s", $code);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt, $code);
+
+echo "<table>\n";
+while(mysqli_stmt_fetch($stmt)){
+
+   echo "<tr>\n";
+   echo "<td>$code</td>";
+   echo "</tr>\n";
+
+}
+echo "</table>\n";
+
+?>
