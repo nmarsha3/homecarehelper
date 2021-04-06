af751c331e53afc60ce4e819c95ff8ad40106739
Nicholas Marshall <nmarsha3@db.cse.nd.edu>
2021-04-06 15:46:05 -0400

    Able to insert data into database from frontend

--- frontend/form_handler.php
+++ frontend/form_handler.php
@@ -1,19 +1,23 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 echo "This is the form handler. username and password inputted: \n";
-$username = $_POST['username'];
-echo "$username\n";
-$password = $_POST['password'];
-echo "$password\n";
+$username = $_GET['input-username'];
+echo $username;
+$password = $_GET['input-password'];
+echo $password;
 
 $link = mysqli_connect('localhost', 'mrauch2', 'frc254') or die ('died');
 
 mysqli_select_db($link, 'mrauch2');
 
-$stmt = mysqli_prepare($link, "insert into logins (username) values (?, ?)");
+echo "We selected the db\n";
+
+$stmt = mysqli_prepare($link, "insert into logins (username, password) values (?, ?)");
 mysqli_stmt_bind_param($stmt, "ss", $username, $password);
 mysqli_stmt_execute($stmt);
 mysqli_stmt_close($stmt);
+
+echo "We hypothetically inserted into the db\n";
 
 #$stmt = mysqli_prepare($link, "insert into logins (type) values (?)");
 #mysqli_stmt_bind_param($stmt, "i", "Patient");
