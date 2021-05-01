07078dad0f48a32e8b9533f115256a9495bb55ac
Greta Rauch <mrauch2@nd.edu>
2021-05-01 17:01:17 -0400

    trying to insert into db - not working at all

--- frontend/new_user_handler.php
+++ frontend/new_user_handler.php
@@ -2,6 +2,11 @@
 
 include "config.php";
 
+$fn = mysqli_real_escape_string($con, $_GET['input-first-name']);
+echo $fn;
+$ln = mysqli_real_escape_string($con, $_GET['input-last-name']);
+echo $ln;
+$bday = mysqli_real_escape_string($con, $_GET['input-birthday']);
 $ssn = mysqli_real_escape_string($con, $_GET['input-ssn']);
 $addr = mysqli_real_escape_string($con, $_GET['input-address']);
 $city = mysqli_real_escape_string($con, $_GET['input-city']);
@@ -10,12 +15,19 @@ $zip = mysqli_real_escape_string($con, $_GET['input-zip']);
 $phone = mysqli_real_escape_string($con, $_GET['input-phone']);
 
 # TODO: Insert above information into db!!
-$stmt = mysqli_prepare($con, "select name from Procedures where code = ?");
-mysqli_stmt_bind_param($stmt, "s", $code);
+$stmt = mysqli_prepare($con, "insert into People values(?, ?, ?, ?)");
+mysqli_stmt_bind_param($stmt, "ssss", $snn, $fn, $ln, $bday);
 mysqli_stmt_execute($stmt);
-mysqli_stmt_bind_result($stmt, $code);
+mysqli_stmt_close($stmt);
 
-mysqli_stmt_fetch($stmt);
-echo $code;
+$stmt2 = mysqli_prepare($con, "insert into Patient values(?, ?, ?, ?, ?)");
+mysqli_stmt_bind_param($stmt2, "sssss", $snn, $addr, $city, $counry, $zip);
+mysqli_stmt_execute($stmt2);
+mysqli_stmt_close($stmt2);
+
+$stmt3 = mysqli_prepare($con, "insert into Phone values(?, ?)");
+mysqli_stmt_bind_param($stmt3, "ss", $snn, $phone);
+mysqli_stmt_execute($stmt3);
+mysqli_stmt_close($stmt3);
 
 ?>
