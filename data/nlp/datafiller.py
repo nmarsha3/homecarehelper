96bd3d518c6e9dd2070596d22d9b3c94dc9d42cb
Jacob Mazur <jmazur2@nd.edu>
2021-05-08 16:18:25 -0400

    NLP data :DDD

---
+++ data/nlp/datafiller.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import random
+
+def readDataFile(filename, attribute, data):
+   '''Reads in an input data file and adds the data vals to the data dict under the specified attribute
+         filename = File to read from
+         attribute = Name of attribute to add data for
+         data = Dict to add data to'''
+
+   if filename is None or attribute is None or data is None:
+      return
+
+   print(f'\tReading attribute \"{attribute}\" from \"{filename}\"...')
+
+   data[attribute] = list()
+
+   with open(filename, "r") as inFile:
+      for line in inFile:
+         data[attribute].append(line.strip())
+
+def main():
+   if len(sys.argv) != 3:
+      print('Error: invalid args')
+      sys.exit(1)
+
+   tempfile = sys.argv[1]
+   outfile = sys.argv[2]
+
+   if os.path.exists(outfile):
+      os.remove(outfile)
+
+   data = {}
+   readDataFile('../data_files/ssns.txt', 'ssn', data)
+   readDataFile('../data_files/hospital_names.txt', 'hospital', data)
+   readDataFile('../data_files/hospital_names.txt', 'clinic', data)
+
+   with open(tempfile, 'r') as template, open(outfile, 'w') as output:
+      
+      for line in template:
+         if '(ssn)' in line:
+               line = line.replace('(ssn)', str(random.choice(data['ssn'])))
+         if '(hospital)' in line:
+               line = line.replace('(hospital)', str(random.choice(data['hospital'])))
+         if '(clinic)' in line:
+               line = line.replace('(clinic)', str(random.choice(data['clinic'])))
+
+         output.write(line)
+
+   print('done!')
+
+
+if __name__ == '__main__':
+   main()
